84aedb5ae652f181414d64d170179690be72a48e
KrutalevichLev <krutaleviclev@gmail.com>
2026-05-06 16:56:58 +0300

    task02 work

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -4,3 +4,27 @@
 // Дан вектор целочисленных значений. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая вычисляет среднее арифметическое ненулевых 
 // элементов вектора.
+
+double calculate_arithmetical_mean_of_nonzero_elements(int* array,int size) {
+
+	if (size <= 0 || array == nullptr) {
+		return 0;
+	}
+
+	int count = 0;
+	double sum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] != 0) {
+			count++;
+			sum += array[i];
+		}
+	}
+
+	if (count == 0) {
+		return 0;
+	}
+
+	return sum / count;
+}
--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -4,3 +4,32 @@
 // Дан вектор целочисленных значений. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая подсчитывает количество локальных минимумов 
 // и максимумов.
+
+int count_local_extreme_numbers(int* array, int size) {
+    if (size <= 1) {
+        return 0;
+    }
+
+    int count = 0;
+
+    if (array[0] != array[1]) {
+        count++; 
+    }
+
+   
+    for (int i = 1; i < size - 1; i++) {
+        
+        if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
+            count++;
+        }
+        else if (array[i] < array[i - 1] && array[i] < array[i + 1]) {
+            count++;
+        }
+    }
+
+    if (array[size - 1] != array[size - 2]) {
+        count++; 
+    }
+
+    return count;
+}
