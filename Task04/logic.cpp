94c08b2b23c015f361ef0a2ae847a01b97b7bd45
KrutalevichLev <krutaleviclev@gmail.com>
2026-05-06 17:49:59 +0300

    task04 work

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -4,3 +4,54 @@
 // Дан вектор натуральных чисел. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая вычисляет среднее рифметическое элементов
 // вектора без учёта экстремальных элементов.
+#include "logic.h"
+
+int search_max_elements(int* array, int size) {
+	int max = *array;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (*(array + i) > max) {
+			max = *(array + i);
+		}
+	}
+
+	return max;
+}
+
+int search_min_elements(int* array, int size) {
+	int min = *array;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (*(array + i) < min) {
+			min = *(array + i);
+		}
+	}
+
+	return min;
+}
+
+
+double calculate_arithmetical_mean_of_nonextreme_elements(int* array, int size) {
+	double sum = 0;
+	int counter = 0;
+
+	if (size <= 0 || array == nullptr) {
+		return 0;
+	}
+
+	int max = search_max_elements(array, size);
+	int min = search_min_elements(array, size);
+
+
+	for (int i = 0; i < size; i++)
+	{
+		if (*(array + i) != max && *(array + i) != min) {
+			sum += *(array + i);
+			counter++;
+		}
+	}
+
+	return  counter != 0 ? sum / counter : 0;
+}
